2ef969cf7e1bf7837243ecd3e6157740350f28a0
franciscosacouto <francisco.sa.couto42@gmail.com>
2025-11-13 14:48:14 +0000

    changes

--- setup_medClipInsights.py
+++ setup_medClipInsights.py
@@ -2,11 +2,8 @@ import os
 from transformers import AutoProcessor, AutoModel
 import torch
 from PIL import Image
-import nrrd
 import numpy as np
 import sys
-import torch
-import torch
 import base64
 import io
 
@@ -14,25 +11,26 @@ import pandas as pd
 
 
 
-def search_files(rootdir,df):
-    for  file in os.walk(rootdir):
-        print(os.path.join(file))
-        new_row ={ "PID": file.split('_')[0], "file_path": os.path.join( file)}
-        #attach file name to df
-        df.append(new_row, ignore_index=True)
-
+def search_files(rootdir, df):
+    for dirpath, _, filenames in os.walk(rootdir):
+        for filename in filenames:
+            full_path = os.path.join(dirpath, filename)
+            pid = filename.split('_')[0]  # get PID from filename
+            new_row = {"PID": pid, "file_path": full_path}
+            df.loc[len(df)] = new_row
+            print(f"Found file: {full_path}")
     return df
 
 
 
-def get_slices_3d(volume, every_n=1):
-    slices = []
-    # Iterate over the depth axis (axis=3)
-    for i in range(0, volume.shape[2], every_n):
-        slice_img = (volume[:, :, i] * 255).astype(np.uint8)
-        pil_img = Image.fromarray(slice_img).convert("RGB")
-        slices.append(pil_img)
-    return slices
+# def get_slices_3d(volume, every_n=1):
+#     slices = []
+#     # Iterate over the depth axis (axis=3)
+#     for i in range(0, volume.shape[2], every_n):
+#         slice_img = (volume[:, :, i] * 255).astype(np.uint8)
+#         pil_img = Image.fromarray(slice_img).convert("RGB")
+#         slices.append(pil_img)
+#     return slices
 
 sys.path.insert(1, '/nas-ctm01/homes/fmferreira/MedImageInsights')
 from medimageinsightmodel import MedImageInsight
@@ -67,7 +65,7 @@ def get_embedding(pil_img_path, classifier):
 
 def get_embeddings_from_dataframe(df, classifier):
     embeddings = {}
-    for row in df.iterrows():
+    for _, row in df.iterrows():
         pid = row['PID']
         file_path = row['file_path']
         emb = get_embedding(file_path, classifier)
