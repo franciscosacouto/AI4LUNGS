3fff11bee9dfa6a481c0f8c6a11ed971e902716a
franciscosacouto <francisco.sa.couto42@gmail.com>
2025-11-13 15:31:32 +0000

    changes

--- setup_medClipInsights.py
+++ setup_medClipInsights.py
@@ -75,7 +75,7 @@ def get_embeddings_from_dataframe(df, classifier):
 #main code
 def main():
 
-    df= pd.DataFrame()
+    df= pd.DataFrame( columns=["PID", "file_path"])
     classifier.load_model()
 
     rootdir_lung = '/nas-ctm01/datasets/public/medical_datasets/lung_ct_datasets/nlst/preprocessed_data/protocol_5/2d/lung'
